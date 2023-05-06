48536900027a7f8b5474cf1c74d44f25663b3eff
redpillar <kgy743@naver.com>
2023-05-07 00:33:50 +0900

    dk

--- js/common/ui/common.js
+++ js/common/ui/common.js
@@ -22,4 +22,48 @@ _chart.forEach((c,i)=>{
 //     newDeg[1]+'deg '+newDeg[2]+'deg, #ff6a93 '+
 //     newDeg[2]+'deg )';
 }
+
+function getHalfNoodle(el){
+    const _this = (el.classList.contains("half-chart"))?el:null;
+    if(!_this) return;
+    const _needle = _this.querySelector(".half-chart-needle");
+    const _txt = _this.querySelector(".half-chart-txt");
+    const getPer = ()=>{
+        const p = (_this.getAttribute("p"))?Number(_this.getAttribute("p").replace(/\D/g,'')):0;
+        const temp = ( p > 100)?100:p;
+        return temp;
+    }
+    const getCounting = ()=>{
+        const totalTime = Number(window.getComputedStyle(_needle)['transition-duration'].replace(/s/g,'')) * 1000;
+        const len = 50;
+        const num = totalTime / len;
+        const onePlus = p / (len - 1);
+        let txt = 0;
+        let time = 0;
+        if(_this.counterTimer) clearSetInterval(_this.counterTimer);
+        _this.counterTimer = setInterval(()=>{
+            time += num;
+            txt = (txt + onePlus > p)?p:txt + onePlus;
+            _txt.innerText = String(txt).replace(/(\.\d+)/g,'') + "%";
+            if(time >= totalTime) clearInterval(_this.counterTimer);
+        },num)
+    }
+    // const 
+    const p = getPer()
+    const deg = 180 * 0.01 * p;
+    const styleRotate = "rotate("+ deg +"deg)";
+    _txt.innerText = "0%";
+    setTimeout(()=>{
+        _needle.style.transform = styleRotate;
+        if(p > 0) getCounting()
+    },500);
+    
+    
+}
+const _halfChart = document.querySelectorAll(".half-chart");
+_halfChart.forEach((c,i)=>{
+    const p = Math.floor(Math.random() * 100);
+    c.setAttribute("p",p);
+    getHalfNoodle(c)
+})
 chartDraw();
